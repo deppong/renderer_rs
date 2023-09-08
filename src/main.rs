657df2655da3125cb805b054fe4e3b34ad82ded0
Isaiah Deppong <isaiahjdeppong@gmail.com>
2023-09-08 19:03:55 -0400

    added tris and drawing algorithm?

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate sdl2;
 // sdl
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::PixelFormatEnum;
+use sdl2::pixels::{PixelFormatEnum, Color};
 
 // mine
 use linear_math::*;
@@ -44,7 +44,7 @@ fn main() {
     let mut loader = ObjLoader::new();
 
     // https://groups.csail.mit.edu/graphics/classes/6.837/F03/models/teapot.obj
-    loader.load_file("cow.obj".to_string());
+    loader.load_file("teddy.obj".to_string());
 
     let mut rx_angle: f32 = 0.00;
     let mut ry_angle: f32 = 0.00;
@@ -104,17 +104,29 @@ fn main() {
         framedata = vec![0; ((WIDTH*HEIGHT)*4) as usize];
 
         for face in &mut loader.faces {
-            for j in 0..3 {
-                let v0 = model * loader.verts[face[j] as usize];
-                let v1 = model * loader.verts[face[(j + 1) % 3] as usize];
-                let line = RLine {
-                    x0: v0.x,
-                    x1: v1.x,
-                    y0: v0.y,
-                    y1: v1.y,
-                };
-                line.draw(&mut framedata, WIDTH, HEIGHT);
-            }
+            // for j in 0..3 {
+            //     let v0 = model * loader.verts[face[j] as usize];
+            //     let v1 = model * loader.verts[face[(j + 1) % 3] as usize];
+            //     let line = RLine {
+            //         x0: v0.x,
+            //         x1: v1.x,
+            //         y0: v0.y,
+            //         y1: v1.y,
+            //         color: Color::WHITE
+            //     };
+
+            //     line.draw(&mut framedata, WIDTH, HEIGHT);
+            // }
+            let v0 = model * loader.verts[face[0] as usize];
+            let v1 = model * loader.verts[face[1] as usize];
+            let v2 = model * loader.verts[face[2] as usize];
+            let mut tri = Triangle {
+                v0: Point{  x: v0.x, y: v0.y},
+                v1: Point{  x: v1.x, y: v1.y},
+                v2: Point{  x: v2.x, y: v2.y},
+            };
+            tri.draw(&mut framedata, WIDTH, HEIGHT);
+
         }
 
         canvas.clear();
--- src/shapes.rs
+++ src/shapes.rs
@@ -6,7 +6,7 @@ pub fn put_pixel(x: f32, y: f32, color: Color, framedata: &mut Vec<u8>, width: u
     let x: u32 = (x + 1. * (width as f32)/2.0) as u32;
     let y: u32 = (y + 1. * (height as f32)/2.0) as u32;
 
-    if x >= width || y >= height {
+    if x >= width || y >= height || x < 0 || y < 0 {
         return;
     }
 
@@ -17,9 +17,9 @@ pub fn put_pixel(x: f32, y: f32, color: Color, framedata: &mut Vec<u8>, width: u
 }
 
 #[derive(Clone, Copy)]
-pub struct RPoint {
-    pub transform: Vec4f,
-    pub color: Color,
+pub struct Point {
+    pub x: f32,
+    pub y: f32
 }
 
 pub struct RLine {
@@ -27,6 +27,14 @@ pub struct RLine {
     pub x1: f32,
     pub y0: f32,
     pub y1: f32,
+    pub color: Color
+}
+
+#[derive(Clone, Copy)]
+pub struct Triangle {
+    pub v0: Point,
+    pub v1: Point,
+    pub v2: Point,
 }
 
 impl RLine {
@@ -34,20 +42,20 @@ impl RLine {
     pub fn draw(&self, framedata: &mut Vec<u8>, width: u32, height: u32) {
         if (self.y1 - self.y0).abs() < (self.x1 - self.x0).abs() {
             if self.x0 > self.x1 {
-                Self::draw_low(framedata, width, height, self.x1, self.y1, self.x0, self.y0);
+                Self::draw_low(self, framedata, width, height, self.x1, self.y1, self.x0, self.y0);
             } else {
-                Self::draw_low(framedata, width, height, self.x0, self.y0, self.x1, self.y1);
+                Self::draw_low(self, framedata, width, height, self.x0, self.y0, self.x1, self.y1);
             }
         } else {
             if self.y0 > self.y1 {
-                Self::draw_high(framedata, width, height, self.x1, self.y1, self.x0, self.y0);
+                Self::draw_high(self, framedata, width, height, self.x1, self.y1, self.x0, self.y0);
             } else {
-                Self::draw_high(framedata, width, height, self.x0, self.y0, self.x1, self.y1);
+                Self::draw_high(self, framedata, width, height, self.x0, self.y0, self.x1, self.y1);
             }
         }
     }
 
-    fn draw_low(framedata: &mut Vec<u8>, width: u32, height: u32, x0: f32, y0: f32, x1: f32, y1: f32) {
+    fn draw_low(&self, framedata: &mut Vec<u8>, width: u32, height: u32, x0: f32, y0: f32, x1: f32, y1: f32) {
         let dx = x1 - x0;
         let mut dy = y1 - y0;
 
@@ -61,7 +69,7 @@ impl RLine {
         let mut y = y0;
 
         for x in x0 as i32..x1 as i32 {
-            put_pixel(x as f32, y as f32, Color::WHITE, framedata, width, height);
+            put_pixel(x as f32, y as f32, self.color, framedata, width, height);
             if d > 0.0 {
                 y = y + yi;
                 d = d + (2.0 * (dy - dx));
@@ -71,7 +79,7 @@ impl RLine {
         }
     }
     
-    fn draw_high(framedata: &mut Vec<u8>, width: u32, height: u32, x0: f32, y0: f32, x1: f32, y1: f32) {
+    fn draw_high(&self, framedata: &mut Vec<u8>, width: u32, height: u32, x0: f32, y0: f32, x1: f32, y1: f32) {
         let mut dx = x1 - x0;
         let dy = y1 - y0;
 
@@ -85,13 +93,88 @@ impl RLine {
         let mut x = x0;
 
         for y in y0 as i32..y1 as i32 {
-            put_pixel(x as f32, y as f32, Color::WHITE, framedata, width, height);
+            put_pixel(x as f32, y as f32, self.color, framedata, width, height);
             if d > 0.0 {
                 x = x + xi;
                 d = d + (2.0 * (dx - dy)); 
             } else {
                 d = d + (2.0 * dx);
             }
+        }
+    }
+}
+
+impl Triangle {
+    // Algorithm mostly stolen from here (Great article btw)
+    // http://www.sunshine2k.de/coding/java/TriangleRasterization/TriangleRasterization.html#sunbresenhamarticle
+    pub fn draw(&mut self, framedata: &mut Vec<u8>, width: u32, height: u32) {
+        if self.v0.y > self.v1.y {std::mem::swap(&mut self.v0, &mut self.v1)}
+        if self.v0.y > self.v2.y {std::mem::swap(&mut self.v0, &mut self.v2)}
+        if self.v1.y > self.v2.y {std::mem::swap(&mut self.v1, &mut self.v2)}
+
+        if self.v1.y == self.v2.y {
+            self.flat_bot_tri(framedata, width, height)
+        } else if self.v0.y == self.v1.y {
+            self.flat_top_tri(framedata, width, height)
+        } else {
+            let v3 = Point {
+                x: self.v0.x + ((self.v1.y - self.v0.y)/(self.v2.y - self.v0.y)) * (self.v2.x - self.v0.x),
+                y: self.v1.y
+            };
+            let oldself = self.clone();
+            self.v2 = v3;
+            self.flat_bot_tri(framedata, width, height);
+
+            self.v0 = oldself.v1;
+            self.v1 = v3;
+            self.v2 = oldself.v2;
+        }
+        
+    }
+
+    fn flat_bot_tri(&self, framedata: &mut Vec<u8>, width: u32, height: u32) {
+        let slope1 = (self.v1.x - self.v0.x) / (self.v1.y - self.v0.y);
+        let slope2 = (self.v2.x - self.v0.x) / (self.v2.y - self.v0.y);
+
+        let mut x1 = self.v0.x;
+        let mut x2 = self.v0.x;
+
+        let mut y = self.v0.y.clone();
+        while y <= self.v1.y {
+            let line = RLine {
+                    x0: x1,
+                    x1: x2,
+                    y0: y,
+                    y1: y,
+                    color: Color::RED 
+            };
+            line.draw(framedata, width, height);
+            x1-=slope1;
+            x2-=slope2;
+            y+=1.0;
+        }
+    }
+
+    fn flat_top_tri(&self, framedata: &mut Vec<u8>, width: u32, height: u32) {
+        let slope1 = (self.v2.x - self.v0.x) / (self.v2.y - self.v0.y);
+        let slope2 = (self.v2.x - self.v1.x) / (self.v2.y - self.v1.y);
+
+        let mut x1 = self.v2.x;
+        let mut x2 = self.v2.x;
+
+        let mut y = self.v2.y.clone();
+        while y > self.v0.y {
+            let line = RLine {
+                    x0: x1,
+                    x1: x2,
+                    y0: y,
+                    y1: y,
+                    color: Color::RED 
+            };
+            line.draw(framedata, width, height);
+            x1-=slope1;
+            x2-=slope2;
+            y-=1.0;
         }
     }
 }
